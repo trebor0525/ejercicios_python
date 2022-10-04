356862bf58d8afb2b11de499b34c2167e8875339
trebor0525 <rxe0525@gmail.com>
2022-10-04 17:34:22 -0500

    Calcular el área de un triángulo

---
+++ triangulo.py
@@ -0,0 +1,6 @@
+# Calcular el área de un triángulo 
+
+base = int(input("Ingresar la base del triángulo: "))
+altura = int(input("Ingresar la altura del triángulo: "))
+area = (base * altura) / 2
+print ("Él área del triángulo es: ", area, "cm²")
